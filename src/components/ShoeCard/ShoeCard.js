48d398e6a4518a0272071d9670202d6922015452
Joel Harrison <Joel.Harrison@netscout.com>
2023-02-28 09:29:06 -0800

    Grid Components

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -31,28 +31,54 @@ const ShoeCard = ({
       ? 'new-release'
       : 'default'
 
+  const displayText = (variant === 'on-sale') ? 'Sale' : 'Just Released!';
+
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
+          <Ribbon variant={variant}>{displayText}</Ribbon>
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price
+            style={{
+              '--color':
+                variant === 'on-sale' ? COLORS.gray[700] : undefined,
+              '--decoration':
+                variant === 'on-sale' ? 'line-through' : undefined,
+            }}
+          >
+            {formatPrice(price)}
+          </Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          {variant == 'on-sale' && <SalePrice>{formatPrice(salePrice)}</SalePrice>}
         </Row>
       </Wrapper>
     </Link>
   );
 };
 
+const Ribbon = styled.div`
+  display: ${p => p.variant === "default" ? "none" : "initial"};
+  color: ${COLORS.white};
+  background-color: ${p => p.variant === "new-release" ? COLORS.secondary : COLORS.primary};
+  font: ${WEIGHTS.bold} ${14/16}rem 'Raleway', sans-serif;
+  position: absolute;
+  top: 12px;
+  right: -4px;
+  padding: 9px;
+  border-radius: 2px;
+`;
+
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex: 1 1 344px;
 `;
 
 const Wrapper = styled.article``;
@@ -61,10 +87,15 @@ const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+  border-radius: 16px 16px 4px 4px;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Name = styled.h3`
@@ -72,7 +103,10 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  color: var(--color);
+  text-decoration: var(--decoration);
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
